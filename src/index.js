8c0cc743493431a096d2038433f78c20011165ec
Eric Bernardo <eric.bernardo.sousa@gmail.com>
2020-04-24 09:24:06 -0300

    *

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 
 // core components
 import Admin from "layouts/Admin.js";
+import Auth from "layouts/Auth.js";
 import RTL from "layouts/RTL.js";
 
 import "assets/css/material-dashboard-react.css?v=1.8.0";
@@ -31,9 +32,10 @@ const hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/admin" component={Admin} />
+    <Route path="/login" component={Auth} />
+    <Route path="/admin" component={Admin} />
       <Route path="/rtl" component={RTL} />
-      <Redirect from="/" to="/admin/dashboard" />
+      <Redirect from="/" to="/login" />
     </Switch>
   </Router>,
   document.getElementById("root")
